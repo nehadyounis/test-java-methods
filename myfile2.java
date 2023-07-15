3261e06ff2ec7f235946372e812550c55eacb371
Nehad Younis <67816665+nehadyounis@users.noreply.github.com>
2023-07-15 06:01:07 +0300

    Create myfile2.java

---
+++ myfile2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+  static void myAge(String fname) {
+    System.out.println(fname + " is ");
+  }
+
+  public static void main(String[] args) {
+    myAge("Liam");
+    myAge("Jenny");
+    myAge("Anja");
+  }
+}
